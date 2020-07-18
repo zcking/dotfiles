1951ff8fda6207862e2cbe1918d48d23534bf026
Zachary King <zcking@protonmail.com>
2020-07-18 12:35:28 -0500

    switch to AdoptOpenJDK

--- install/java.sh
+++ install/java.sh
@@ -10,8 +10,10 @@ fi
 asdf plugin add java
 
 # Install versions of Java
-asdf install java openjdk-14.0.1
+#asdf install java openjdk-14.0.1 # Have certain issues with JDK's above 8, and sadly ASDF doesn't have an OpenJDK of 8
+asdf install java adoptopenjdk-8.0.181+13 # so instead using AdoptOpenJDK
 
 # Set global version for default use
-asdf global java openjdk-14.0.1
+#asdf global java openjdk-14.0.1
+asdf global java adoptopenjdk-8.0.181+13
 
