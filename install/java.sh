a72461d203370bbdf08f3e47f0297fcdb5911318
Zachary King <zcking@protonmail.com>
2022-01-27 23:22:00 -0600

    install java with brew; asdf has too many issues on java

--- install/java.sh
+++ install/java.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
 # java.sh
-# Installs Java (OpenJDK) using asdf-vm
+# Installs Java (OpenJDK 8) using homebrew
 
-if ! [ -x "$(command -v asdf)" ]; then
-  ./install/asdf.sh $1
-fi
-
-asdf plugin add java
-
-# Install versions of Java
-#asdf install java openjdk-14.0.1 # Have certain issues with JDK's above 8, and sadly ASDF doesn't have an OpenJDK of 8
-asdf install java adoptopenjdk-8.0.181+13 # so instead using AdoptOpenJDK
-
-# Set global version for default use
-#asdf global java openjdk-14.0.1
-asdf global java adoptopenjdk-8.0.181+13
-
+brew tap AdoptOpenJDK/openjdk
+brew install --cask adoptopenjdk8
